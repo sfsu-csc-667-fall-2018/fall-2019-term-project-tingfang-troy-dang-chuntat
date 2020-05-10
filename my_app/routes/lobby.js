19b7b4d210e57dac3808c4f59ff4852062afc9c2
Chun <chuntatchan123@gmail.com>
2020-05-09 18:09:46 -0700

    Lobby Page Switcher

--- my_app/routes/lobby.js
+++ my_app/routes/lobby.js
@@ -3,8 +3,13 @@ var router = express.Router();
 var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('authenticated/lobby');
+router.get('/:page', function(request, response) {
+	const { page } = request.params;
+	const rooms = [
+		11, 22, 33, 44, 55, 66, 77, 88, 99, 1010, 1111, 1212, 1313, 1414, 1515
+	];
+
+  response.render('authenticated/lobby', { page , rooms});
 });
 
 module.exports = router;
