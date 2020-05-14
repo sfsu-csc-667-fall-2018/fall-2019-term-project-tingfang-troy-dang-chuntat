b9d335c3f976d59b141c49d7a90740ac1e417f83
margonguyen <margonnguyen@gmail.com>
2020-05-13 23:25:37 -0700

    some buttons for game rooms

--- my-app/app.js
+++ my-app/app.js
@@ -35,7 +35,7 @@ app.set('view engine', 'jade');
 
 app.use(logger('dev'));
 app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: false }));
 app.use(require('body-parser').urlencoded({ extended: true }));
 const initializePassport = require("./config/passport");
 
