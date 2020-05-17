7f5088360b33563f3ac68e54488bbb931be288d9
Chun <chuntatchan123@gmail.com>
2020-05-16 18:39:03 -0700

    Updated frontEnd 6:38PM 5/16/2020

--- my_app/routes/index.js
+++ my_app/routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 var path = require('path');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('unauthenticated/index', { title: 'Texas Hold em' });
+router.get('/', function(request, response) {
+  	response.render('unauthenticated/login');
 });
 
 router.get('/users/login', (_, response) => {
